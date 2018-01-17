9f8e5c2c9a2e1f88a9ffe089bf7b6311e637d7ce
saadou11 <saadallah.mahi@gmail.com>
2018-01-17 21:12:38 +0100

    ajout de commentaires

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Classe principale qui charge toutes les composantes et configuration et qui execute l'application SpringBoot 
+ * @author Saadou
+ * 
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = { "controllers"})
 public class Application {
