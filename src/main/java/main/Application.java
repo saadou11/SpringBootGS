2ef7c5697ed5e20341eae08fa12e034f5f69cdd7
saadou11 <saadallah.mahi@gmail.com>
2018-01-29 20:13:55 +0100

    utilisation de l'annotation Bean

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,21 +1,50 @@
 package main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import repositories.CustomerRepository;
+import entities.Customer;
 
 /**
- * Classe principale qui charge toutes les composantes et configuration et qui execute l'application SpringBoot 
+ * Classe principale qui charge toutes les composantes et configuration et qui execute l'application SpringBoot
+ * 
  * @author Saadou
  * 
  */
 @SpringBootApplication
-@ComponentScan(basePackages = { "controllers","entities"})
+@ComponentScan(basePackages = { "controllers", "entities"})
+@EntityScan("entities")
+@EnableJpaRepositories("repositories")
 public class Application {
+	
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
 	}
 
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+			
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+		};
+	}
 }
